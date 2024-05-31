4dc4f2f66da5414db5e965e97bdbbdad992f31f4
UniAllYuki <isaacbrindes41@gmail.com>
2024-05-31 10:42:43 -0300

    Adicionando Lógica de Check-mate

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!cm.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(cm, captured);
@@ -51,7 +51,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
-		
+		UI.clearScreen();
+		UI.printMatch(cm, captured);
 		
 	}
 
--- src/application/UI.java
+++ src/application/UI.java
@@ -58,9 +58,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turno: " + chessMatch.getTurn());
-		System.out.println("Espero pelo jogador: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Espero pelo jogador: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECK-MATE!");
+			System.out.print("Vencedor: " + chessMatch.getCurrentPlayer());
 		}
 	}
 	
