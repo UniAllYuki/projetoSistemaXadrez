930faf6594ddead64f6dcd03c852c0d8ed349683
UniAllYuki <isaacbrindes41@gmail.com>
2024-05-31 11:39:37 -0300

    Adicionado o CAVALO

---
+++ src/chess/pieces/Cavalo.java
@@ -0,0 +1,77 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Cavalo extends ChessPiece {
+
+	public Cavalo(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "C";
+	}
+
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		//Acima	- Esquerda
+		p.setValues(position.getRow() - 2, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		// Esquerda - Acima
+		p.setValues(position.getRow() - 1, position.getColumn() - 2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Esquerda - Abaixo
+		p.setValues(position.getRow() + 1, position.getColumn() - 2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Abaixo - Esquerda
+		p.setValues(position.getRow() + 2, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		// Abaixo - Direita
+		p.setValues(position.getRow() + 2, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Direita - Abaixo
+		p.setValues(position.getRow() + 1, position.getColumn() + 2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Direita - Acima
+		p.setValues(position.getRow() - 1, position.getColumn() +2);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Acima - Direita
+		p.setValues(position.getRow() - 2, position.getColumn() +1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return mat;
+	}
+	
+}
